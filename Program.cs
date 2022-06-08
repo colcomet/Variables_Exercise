84513f824c4ef147c2f9656b55814f58e8dbac86
Andrew Barber <drewbarber72@gmail.com>
2022-06-08 14:25:31 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Variables_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Andrew";
+            int myAge = 50;
+            char myInitial = 'A';
+            bool amIAStudent = true;
+            decimal bankBalance = 56.05M;
+
+            Console.WriteLine($"My name is {myName}, and I am {myAge} years old. My first initial is {myInitial}, and it is {amIAStudent} that I am a student. Finally, my bank balance is currently {bankBalance:$0.00}");
+
+        }
+    }
+}
